b38ebe7be4b68ca37d3731ff4378d7faa57123bf
denghua <denghua.fm@foxmail.com>
2023-04-05 17:13:46 +0800

    feat: basic page element created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Nav from './components/Nav/';
+import Main from './components/Main';
+import SoundSelection from './components/SoundSelection';
+import DisplaySection from './components/DisplaySection';
+import WebgiViewer from './components/WebgiViewer';
 
 function App() {
 
   return (
     <div className="App">
-      Hello
+      <Nav />
+      <Main />
+      <SoundSelection />
+      <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
